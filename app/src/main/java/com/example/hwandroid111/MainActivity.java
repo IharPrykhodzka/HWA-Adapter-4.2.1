bd2c49fc87269d29a42a288d2e56531fbdb503c3
Igor Prykhodzka <Kvait@bk.ru>
2020-03-24 17:29:29 +0300

    Second commit

--- app/src/main/java/com/example/hwandroid111/MainActivity.java
+++ app/src/main/java/com/example/hwandroid111/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.hwandroid111;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -31,45 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
         initList();
     }
-
-
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//
-//        if (id == R.id.action_open_notes) {
-//            Toast.makeText(MainActivity.this, R.string.txtOpenNote, Toast.LENGTH_LONG).show();
-//
-//            Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
-//            startActivity(intentNotes);
-//
-//            return true;
-//
-//        } else if (id == R.id.action_open_address) {
-//            Toast.makeText(MainActivity.this, R.string.txtOpenAddress, Toast.LENGTH_LONG).show();
-//
-//            Intent intentAddress = new Intent(MainActivity.this, AddressActivity.class);
-//            startActivity(intentAddress);
-//
-//            return true;
-//
-//        } else if (id == R.id.action_open_calendsr) {
-//            Toast.makeText(MainActivity.this, R.string.txtOpenCalendar, Toast.LENGTH_LONG).show();
-//
-//            Intent intentCalendar = new Intent(MainActivity.this, CalendarActivity.class);
-//            startActivity(intentCalendar);
-//
-//            return true;
-//
-//        } else if (id == R.id.action_settings) {
-//            Toast.makeText(MainActivity.this, R.string.txtOpenSettings, Toast.LENGTH_LONG).show();
-//
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     private void initList() {
         ListView listView = findViewById(R.id.list_view);
